851da26634e857439d449621f2871c88b34d06ff
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-08 18:33:21 +0000

    Jul 9, 2023, 12:03 AM

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Login() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       This is Login Page.
-      <button className='bg-purple-600 hover:bg-purple-50'>Login</button>
+      <button className='bg-purple-600 hover:bg-purple-700'>Login</button>
     </main>
   )
 }
