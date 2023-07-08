d9e6c2a4d60f4b6b000324c9b8578f68f3cf23cc
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-08 18:22:23 +0000

    Jul 8, 2023, 11:52 PM

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Login() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       This is Login Page.
+      <button>Login</button>
     </main>
   )
 }
