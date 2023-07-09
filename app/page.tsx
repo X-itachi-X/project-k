698a31ef2c0a2b567132209af7d0a9809974990c
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-09 04:31:11 +0000

    Jul 9, 2023, 10:01 AM

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Login() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       const result = print();
-      
+      {print()}
     </main>
   )
 }
