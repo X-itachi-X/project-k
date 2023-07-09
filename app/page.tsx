3f1d402a233417fb171c657271ed98778bbb9cff
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-09 04:22:32 +0000

    Jul 9, 2023, 9:52 AM

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,16 @@ import Image from 'next/image'
 export default function Login() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <
+      print()
       
+    </main>
+  )
+}
+
+function print() {
+  return (
+    <main>
+      This is print funtion
     </main>
   )
 }
